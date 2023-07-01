b15e70a00a9df59601dcbb25357e2d126bca7697
Mesti17 <mesty017@gmail.com>
2023-07-01 16:11:51 +0700

    Add model and prediction features

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
 from flask import Flask, flash, redirect, url_for, render_template, request, send_from_directory
 from search import cariTweet
 import re
+import pickle
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = Flask(__name__, template_folder="templates", static_url_path='/static')
 
+#Load Model
+model_path = 'model/model_nb.pickle'
+model = pickle.load(open(model_path, "rb"))
+
+#Load Vectorizer
+vectorizer = TfidfVectorizer()
+with open('model/vec.pickle', 'rb') as handle:
+    vectorizer = pickle.load(handle)
+
+# def sentimen_tweet(tweet):
+    
+#     teks = tweet
+#     vec = vectorizer.transform([teks])
+#     prediksi = model.predict(vec)
+
+#     if prediksi == 1:
+#         hasil = 'POSITIF'
+#     elif prediksi == 0:
+#         hasil = 'NETRAL'
+#     else:
+#         hasil = 'NEGATIF'
+        
+#     return hasil
 
 @app.route("/")
 def home():
@@ -16,6 +42,7 @@ def prosescari():
     if request.method == "POST":
         message = request.form['hashtag']
         hasil_cari = cariTweet(message)
+        # hasil_pred = cariTweet(message)
 
     return render_template("hasil.html", data=hasil_cari, kata_pencarian=message, total=len(hasil_cari))
 
--- search.py
+++ search.py
@@ -3,8 +3,20 @@ import tweepy
 import mysql.connector
 import re
 import snscrape.modules.twitter as sntwitter
+import pickle
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = Flask(__name__, template_folder="templates", static_url_path='/static')
+
+#Load Model
+model_path = 'model/model_nb.pickle'
+model = pickle.load(open(model_path, "rb"))
+
+#Load Vectorizer
+vectorizer = TfidfVectorizer()
+with open('model/vec.pickle', 'rb') as handle:
+    vectorizer = pickle.load(handle)
 
 def cleanTxt(text):
     text = re.sub('@[A-Za-z0–9]+', '', text)  # Removing @mentions
@@ -17,6 +29,17 @@ def cleanTxt(text):
 
 
 def searchTweets(query):
+    # teks = tweet
+    # vec = vectorizer.transform([teks])
+    # prediksi = model.predict(vec)
+
+    # if prediksi == 1:
+    #     hasil = 'POSITIF'
+    # elif prediksi == 0:
+    #     hasil = 'NETRAL'
+    # else:
+    #     hasil = 'NEGATIF'
+        
     content = []
 
     for i, tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
@@ -25,9 +48,22 @@ def searchTweets(query):
         username = tweet.user.username
         pesan = tweet.content.encode("utf-8")
         pesan_cleaned = cleanTxt(pesan.decode("utf-8"))
+        
+        #prediksi tweet
+        vec = vectorizer.transform([pesan_cleaned])
+        prediksi = model.predict(vec)
+        
+        if prediksi == 1:
+            hasil = 'POSITIF'
+        elif prediksi == 0:
+            hasil = 'NETRAL'
+        else:
+            hasil = 'NEGATIF'
+        
         content.append({
                 'Username': username,
                 'Tweet': pesan_cleaned,
+                'Label': hasil
             })
     
     return content
